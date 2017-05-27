49aa1e685a98371afc9fee69fe49285bd7f8eae8
nikolabai <nikola.vip@foxmail.com>
2017-05-27 15:37:21 +0800

    修改readme

--- readme.txt
+++ readme.txt
@@ -1,2 +1,7 @@
 简单论坛项目
-技术：ssh数据库 mysql
+技术：
+spring 4.3.7
+hibernate5.2.1
+springMVC
+Mysql5.6
+
--- src/main/java/cn/gavin/dao/impl/BoradDaoImpl.java
+++ src/main/java/cn/gavin/dao/impl/BoradDaoImpl.java
@@ -1,5 +1,10 @@
 package cn.gavin.dao.impl;
 
+import java.io.Serializable;
+import java.util.List;
+
+import org.hibernate.Session;
+
 import cn.gavin.dao.BoardDao;
 
 /**
@@ -8,6 +13,66 @@ import cn.gavin.dao.BoardDao;
  * @2017年3月28日
  */
 
-public class BoradDaoImpl extends BaseDaoImpl implements BoardDao{
+public class BoradDaoImpl extends BaseDaoImpl {
+
+	@Override
+	public Session getSession() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object load(Serializable id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object get(Serializable id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object loadAll(Serializable id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(Object entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove(Object entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void undate(Object entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List find(String hql) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List find(String hql, Object params) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void initializae(Object entity) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
