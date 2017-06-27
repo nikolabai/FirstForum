bb9e403d10858ff5b7d48d9681a6edaa09390986
nikolabai <nikola.vip@foxmail.com>
2017-06-27 10:57:02 +0800

    welcome-register.jsp-submit action-controller-return success

--- src/main/java/cn/gavin/web/RegisterController.java
+++ src/main/java/cn/gavin/web/RegisterController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import cn.gavin.domain.User;
@@ -25,22 +26,26 @@ public class RegisterController extends BaseController{
 	private UserService userService;
 	private static final Logger logger = LoggerFactory.getLogger(RegisterController.class);
 	//用户注册
-	@RequestMapping(value="/register",method=RequestMethod.POST)
-	public ModelAndView register(HttpServletRequest request,User user){
+	@RequestMapping(value="/register")
+	public ModelAndView register(@RequestParam("userName") String userName, @RequestParam("password") String password){
 		logger.info("调用controller");
+		System.out.println(userName+password);
 		ModelAndView view =new ModelAndView();
-		try {
-			userService.register(user);
-		} catch (UserExistsException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			view.addObject("errorMsg","用户名已经存在，请选择其他的名字。");
-			view.setViewName("forward:/register.jsp");
-		}
-		setSessionUser(request, user);
+//		try {
+////			userService.register(user);
+//		} catch (UserExistsException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//			view.addObject("errorMsg","用户名已经存在，请选择其他的名字。");
+//			view.setViewName("forward:/register.jsp");
+//		}
+////		setSessionUser(request, user);
 		return new ModelAndView("success");
 		
 	}
-	
+	@RequestMapping(value="/index",method=RequestMethod.GET) 
+	public String index(){ 
+	return "register"; 
+	}
 
 }
